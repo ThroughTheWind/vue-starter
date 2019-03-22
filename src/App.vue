2c93c137f9cdde730c2934aa733ab0efd4cff459
ThroughTheWind <antoine.dcr@outlook.fr>
2019-03-22 21:41:31 +0100

    Add vuejs-logger

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,31 @@
 
 
 <script>
+import Vue from "vue";
 export default {
   name: "App",
   data() {
     return {
-      //
+      a: "debug log test",
+      b: "debug info test",
+      c: {
+        firstvalue: "testing",
+        secondvalue: 1999
+      }
     };
+  },
+  created() {
+    this.$log.debug("test", this.a, 123);
+    this.$log.info("test", this.b, 7777, this.c);
+    this.$log.warn("test");
+    this.$log.error("test");
+    this.$log.fatal("test");
+    externalFunction();
   }
 };
+
+function externalFunction() {
+  // log from external function
+  Vue.$log.debug("log from function outside component.");
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -7,12 +7,30 @@ import "./registerServiceWorker";
 import _ from "lodash";
 import config from "./config";
 import i18n from "./i18n";
+import VueLogger from "vuejs-logger";
 
+//Custom css
 require("./assets/flat-flags/css/main.css");
+
+const isProduction = process.env.NODE_ENV === "production";
 
 //define lodash globally, can now be used using this.$_
 Object.defineProperty(Vue.prototype, "$_", { value: _ });
+//define config globally, can now be used using this.$config
 Object.defineProperty(Vue.prototype, "$config", { value: config });
+
+//define logger options
+const loggerOptions = {
+  isEnabled: true,
+  logLevel: isProduction ? "error" : "debug",
+  stringifyArguments: false,
+  showLogLevel: true,
+  showMethodName: true,
+  separator: "|",
+  showConsoleColors: true
+};
+
+Vue.use(VueLogger, loggerOptions);
 
 Vue.config.productionTip = false;
 
